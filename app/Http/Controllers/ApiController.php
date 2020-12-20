557142601fa619261a91b95f577b894745e68e1a
Rafael H Liost <rafael.lisot@unoesc.edu.br>
2020-12-19 23:29:15 -0300

    Criando tarefa, listando tarefas, listando uma tarefa e atualizando tarefa

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,23 +3,92 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Todo;
+
+//use Validator;
 
 class ApiController extends Controller
 {
-    public function createtodo(){
+    public function createtodo(Request $request){
+        $array = ['error' => ''];
 
+        $rules = [
+            'title' => 'required|min:3'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $title = $request->input('title');
+
+        $todo = new Todo();
+        $todo->title = $title;
+        $todo->save();
+
+        return $array;
     }
 
     public function readalltodos(){
+        $array = ['error' => ''];
 
+        $array['list'] = Todo::all();
+
+        return $array;
     }
 
-    public function readtodo(){
+    public function readtodo($id){
+        $array = ['error' => ''];
 
+        $todo = Todo::find($id);
+
+        if($todo) {
+            $array['todo'] = $todo;
+        } else {
+            $array['error'] = 'A tarefa '.$id.' não existe!';
+        }
+
+        return $array;
     }
 
-    public function updatetodo(){
+    public function updatetodo($id, Request $request){
+        $array = ['error' => ''];
 
+        $rules = [
+            'title' => 'min:3',
+            'done' => 'boolean'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $title = $request->input('title');
+        $done = $request->input('done');
+
+        $todo = Todo::find($id);
+        if ($todo) {
+            if($title) {
+                $todo->title = $title;
+            }
+            if ($done) {
+                $todo->done = $done;
+            }
+
+            $todo->save();
+
+        } else {
+            $array['error'] = 'Tarefa '.$id.' não existe';
+        }
+
+        return $array;
     }
 
     public function deletetodo(){
